a9ebf01270ac979cf1e684da9700373453c1d996
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-16 10:56:29 -0300

    002

--- app/Livewire/MainComponent.php
+++ app/Livewire/MainComponent.php
@@ -2,10 +2,14 @@
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Title;
 use Livewire\Component;
 
 class MainComponent extends Component
 {
+    // adiciona um tito na aba do navegador (pagina web)
+    #[Title('Livewire Contacts')]
+
     public function render()
     {
         return view('livewire.main-component');
