135fe37459f69e1747c7f474bdd35d066bef4c3f
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-16 11:56:42 -0300

    006

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -3,28 +3,28 @@
 namespace App\Livewire;
 
 use Illuminate\Support\Facades\Log;
+use Livewire\Attributes\Validate;
 use Livewire\Component;
 
 class FormContact extends Component
 {
-    public $name, $email, $phone;
+    #[Validate('required|min:3|max:50')]
+    public $name;
+
+    #[Validate('required|email|min:5|max:50')]
+    public $email;
+
+    #[Validate('required|min:5|max:20')]
+    public $phone;
 
     public function newContact()
     {
         // validation
-        $this->validate([
-            'name' => 'required|min:3|max:50',
-            'email' => 'required|email|min:5|max:50',
-            'phone' => 'required|min:5|max:20',
-        ]);
+        $this->validate();
+
 
         // temporay store in log file
         Log::info('Novo contacto: ' . $this->name . ' - ' . $this->email . ' - ' . $this->phone);
-
-        // // Option 1 - clear form
-        // $this->name = '';
-        // $this->email = '';
-        // $this->phone = '';
 
         // Option 2 
         $this->reset();
