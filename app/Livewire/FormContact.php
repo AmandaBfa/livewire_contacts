8c730f70e1ae34fa1bb6b1b88400acc1f897e8fd
Amanda Barbosa <amandinhabarbosa2005@gmail.com>
2025-09-16 11:31:00 -0300

    004

--- app/Livewire/FormContact.php
+++ app/Livewire/FormContact.php
@@ -2,10 +2,26 @@
 
 namespace App\Livewire;
 
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class FormContact extends Component
 {
+    public $name, $email, $phone;
+
+    public function newContact()
+    {
+        // validation
+        $this->validate([
+            'name' => 'required|min:3|max:50',
+            'email' => 'required|email|min:5|max:50',
+            'phone' => 'required|min:5|max:20',
+        ]);
+
+        // temporay store in log file
+        Log::info('Novo contacto: ' . $this->name . ' - ' . $this->email . ' - ' . $this->phone);
+    }
+
     public function render()
     {
         return view('livewire.form-contact');
